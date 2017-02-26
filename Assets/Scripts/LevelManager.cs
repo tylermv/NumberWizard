9498e38c868f304350b3e1a830e72bbb1f79d56f
tylermv <tyler.jmv@gmail.com>
2017-02-26 02:55:03 -0500

    Initial game load
    Initial game load for number guessing game

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log ("Level load requested for " +name);
+		Application.LoadLevel (name);
+	}
+	public void QuitRequest(string name) {
+		Debug.Log ("Quit game requested for ");
+		Application.Quit();
+	}
+}
---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+// Use this for initialization
+
+	int max;
+	int min;
+	int guess;
+	int maxGuessesAllowed = 11;
+
+	public Text text;
+
+	void Start () {
+		StartGame ();
+	}
+
+	void StartGame () {
+		
+		max = 1000;
+		min = 1;
+		NextGuess ();
+	}
+
+	void RandomGuess () {
+		guess = Random.Range (1, 1000);
+	}
+		
+	void NextGuess () {
+		guess = Random.Range(max,min+1);
+		text.text = guess.ToString ();
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		if (maxGuessesAllowed <= 0) {
+			Application.LoadLevel ("Win");
+		}
+	}
+	public void GuessHigher () {
+		min = guess;
+		NextGuess();
+	}
+	public void GuessLower () {
+		max = guess;
+		NextGuess();
+		}
+	}
